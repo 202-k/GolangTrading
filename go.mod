21d41c8688a48c205bbb3574bf8ec2c6d1415489
DESKTOP-34VBR3C\wns41 <s501074930110@gmail.com>
2022-01-13 22:56:28 +0900

    let's try

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.17
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
 	github.com/sangx2/upbit v0.0.0-20211015065425-a0e59dbf53e9 // indirect
+	github.com/slack-go/slack v0.10.1 // indirect
 )
--- trading.go
+++ trading.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"github.com/sangx2/upbit"
+	"github.com/sangx2/upbit/model/exchange"
 	"github.com/sangx2/upbit/model/quotation"
+	"github.com/slack-go/slack"
+	"log"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 )
 
 // recentFall can be removed
 type coin struct {
 	name         string
+	holdings     bool
+	volume       float64
 	recentFall   bool
 	tradable     bool
-	tradabletime time.Time
-	buyTime      time.Time
+	tradableTime time.Time
+	orderTime    time.Time
+	uuid         string
 }
 
-func (c *coin) EncounterFalling(t string) {
+func (c *coin) EncounterFalling(t time.Time) {
 	c.recentFall = true
-	changedTime := ChangeStringToTime(t)
-	c.tradabletime = changedTime.Add(time.Hour * 24)
+	c.tradableTime = t.Add(time.Hour * 24)
 	c.tradable = false
 }
 
@@ -38,7 +46,7 @@ func (c *coin) GetRecentFall(u upbit.Upbit) {
 	time.Sleep(100000000)
 	candles, _, err := u.GetMinuteCandles(c.name, "", "200", "10")
 	for err != nil {
-		time.Sleep(100000000)
+		time.Sleep(200000000)
 		candles, _, err = u.GetMinuteCandles(c.name, "", "200", "10")
 	}
 	// check recent price falling
@@ -58,7 +66,8 @@ func (c *coin) GetRecentFall(u upbit.Upbit) {
 	for i := 180; i > 0; i-- { // 180~199, 179~198
 		recentAvg[i] = sum / 20
 		if recentAvg[i]*0.97 > recentClose[i] {
-			c.EncounterFalling(recentTime[i])
+			t := ChangeStringToTime(recentTime[i])
+			c.EncounterFalling(t)
 			break
 		}
 		sum -= recentClose[i+19]
@@ -66,9 +75,22 @@ func (c *coin) GetRecentFall(u upbit.Upbit) {
 	}
 }
 
-func ChangeStringToTime(s string) time.Time {
-	t, _ := time.Parse("2006-01-02T15:04:05", s)
-	return t
+func GetWallet(u *upbit.Upbit, coins map[string]*coin) {
+	var err error
+	account, _, err := u.GetAccounts()
+	if err != nil {
+		log.Fatal("GetWallet", err)
+	}
+	for _, a := range account {
+		if a.Currency != "KRW" {
+			name := "KRW-" + a.Currency
+			coins[name].holdings = true
+			coins[name].volume, err = strconv.ParseFloat(a.Balance, 32)
+			if err != nil {
+				fmt.Println("Err in GetWallet : ", err)
+			}
+		}
+	}
 }
 
 func GetKRWMarkets(u *upbit.Upbit) []*quotation.Market {
@@ -80,8 +102,101 @@ func GetKRWMarkets(u *upbit.Upbit) []*quotation.Market {
 				krw = append(krw, market)
 			}
 		}
+	} else {
+		fmt.Println("Err in GetKRWMarkets : ", err)
 	}
 	return krw
+}
+
+func (c *coin) CheckTradablility() {
+	if c.tradableTime.Before(time.Now()) {
+		c.tradable = true
+	}
+}
+
+func (c *coin) CheckCoinStatus(u *upbit.Upbit) (string, float64) {
+	c.CheckTradablility()
+	if c.tradable {
+		time.Sleep(200000000)
+		candle, _, err := u.GetMinuteCandles(c.name, "", "20", "10")
+		num := len(candle)
+		if num == 20 {
+			if err != nil {
+				fmt.Println("Err in CheckCoinStatus : ", err)
+			}
+			var sum float64
+			for i := 0; i < num; i++ {
+				sum += candle[i].TradePrice
+			}
+			currentPrice := candle[0].TradePrice
+			if currentPrice >= sum/20*1.03 {
+				return "buy", currentPrice
+			} else if currentPrice <= sum/20*0.97 {
+				return "sell", currentPrice
+			}
+		}
+	}
+	return "keep", -1
+}
+
+func (c *coin) BuyOrder(u *upbit.Upbit, price float64, amount float64) {
+	v := amount / price
+	volume := strconv.FormatFloat(v, 'f', -1, 32)
+	p := strconv.FormatFloat(price, 'f', -1, 32)
+	order, _, err := u.PurchaseOrder(c.name, volume, p, exchange.ORDER_TYPE_LIMIT, "")
+	if err != nil {
+		log.Fatal("BuyOrder", err)
+	}
+	c.volume += v
+	c.uuid = order.UUID
+	c.tradable = false
+	c.orderTime = time.Now()
+	c.tradableTime = c.orderTime.Add(time.Minute * 30)
+	time.Sleep(200000000)
+}
+
+func (c *coin) SellOrder(u *upbit.Upbit, amount float64) {
+	v := c.volume * amount
+	volume := strconv.FormatFloat(v, 'f', -1, 32)
+	u.SellOrder(c.name, volume, "", exchange.ORDER_TYPE_MARKET, "")
+	c.volume -= v
+	c.EncounterFalling(time.Now())
+	time.Sleep(200000000)
+}
+
+func (c *coin) CheckOrderResult(u *upbit.Upbit) {
+	order, _, err := u.GetOrder(c.uuid, "")
+	if err != nil {
+		log.Fatal("CheckOrderResult", err)
+	}
+	if order.State == exchange.ORDER_STATE_WAIT && time.Now().After(c.orderTime.Add(time.Minute*3)) {
+		u.CancelOrder(c.uuid, "")
+		c.tradable = true
+		c.tradableTime = time.Now()
+		v, _ := strconv.ParseFloat(order.Volume, 32)
+		c.volume -= v
+	}
+	time.Sleep(200000000)
+}
+
+func TradeCoin(u *upbit.Upbit, coins []*coin) {
+	for _, c := range coins {
+		action, price := c.CheckCoinStatus(u)
+		if action == "buy" {
+			c.BuyOrder(u, price, 6000)
+
+		} else if action == "sell" {
+			c.SellOrder(u, 1)
+		} else if time.Now().Before(c.orderTime.Add(time.Minute * 3)) {
+			c.CheckOrderResult(u)
+		}
+		//fmt.Println(c.name, action)
+	}
+}
+
+func ChangeStringToTime(s string) time.Time {
+	t, _ := time.Parse("2006-01-02T15:04:05", s)
+	return t
 }
 
 func main() {
@@ -89,47 +204,98 @@ func main() {
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 	f, err := os.Open("key")
-	fmt.Fprintln(writer, err)
+	if err != nil {
+		fmt.Fprintln(writer, err)
+	}
 	scanner := bufio.NewScanner(f)
 	scanner.Scan()
 	access := scanner.Text()
 	scanner.Scan()
 	secret := scanner.Text()
+	f.Close()
 	// get my upbit account
 	u := upbit.NewUpbit(access, secret)
-	// check coins I have
-	account, _, _ := u.GetAccounts()
-	for _, a := range account {
-		fmt.Fprintln(writer, a)
-	}
+
 	markets := GetKRWMarkets(u)
 	coins := make(map[string]*coin)
+	coingroup := make(map[int][]*coin)
 	// when execute first
+	count := 0
 	for _, market := range markets {
 		name := market.Market
-		coins[name] = &coin{name: name, recentFall: false, tradable: true}
+		coins[name] = &coin{name: name, holdings: false, volume: 0, recentFall: false, tradable: true}
 		coins[name].GetRecentFall(*u)
+		coingroup[count] = append(coingroup[count], coins[name])
+		count += 1
+		count %= 5
 	}
 
-	//for s, c := range coins {
-	//	fmt.Fprintf(writer, "%s : %v\n", s, c)
+	// check coins I have
+	GetWallet(u, coins)
+	//for _, c := range coins {
+	//	fmt.Fprintln(writer, c)
 	//}
+	//fmt.Fprintln(writer, "!!!!!")
 
-	// change down here with function with go routine
-	// look for the rising coins
-	//for {
-	//	now := time.Now()
-	//	for _, c := range coins {
-	//		// check tradability
-	//		if c.tradable == false { // found fall or bought in near time
-	//			// check tradable time
-	//			if now.After(c.tradabletime) {
-	//				c.MakeTradable()
-	//			}
-	//		}
-	//		// find buy strategy
-	//
-	//	}
-	//}
+	f, err = os.Open("slack")
+	if err != nil {
+		fmt.Fprintln(writer, err)
+	}
+	scanner = bufio.NewScanner(f)
+	scanner.Scan()
+	token := scanner.Text()
+	api := slack.New(token)
+
+	var wait sync.WaitGroup
+	file, err := os.OpenFile("./records.csv", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	wr := csv.NewWriter(bufio.NewWriter(file))
+	defer wr.Flush()
+	sent := false
+	// find coins to buy or sell
+	for {
+		wait.Add(5)
+		go func() {
+			defer wait.Done()
+			TradeCoin(u, coingroup[0])
+		}()
+		go func() {
+			defer wait.Done()
+			TradeCoin(u, coingroup[1])
+		}()
+		go func() {
+			defer wait.Done()
+			TradeCoin(u, coingroup[2])
+		}()
+		go func() {
+			defer wait.Done()
+			TradeCoin(u, coingroup[3])
+		}()
+		go func() {
+			defer wait.Done()
+			TradeCoin(u, coingroup[4])
+		}()
+		now := time.Now()
+		if now.Minute()%30 == 0 && sent == false {
+			account, _, _ := u.GetAccounts()
+			for _, a := range account {
+				wr.Write([]string{now.String(), a.Currency, a.Balance})
+				text := "avg buy price : " + a.AvgBuyPrice + ", total : " + a.Balance
+				attachment := slack.Attachment{
+					Title: a.Currency,
+					Text:  text,
+				}
+				api.PostMessage("coin", slack.MsgOptionText("", false),
+					slack.MsgOptionAttachments(attachment))
+			}
+			sent = true
+		} else if now.Minute()%14 == 0 {
+			sent = false
+		}
+		wait.Wait()
+	}
 
 }
